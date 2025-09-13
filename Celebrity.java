680eae3956ac00e56014b16cbdf47908cf0dadfc
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 17:12:01 +0800

    oui

--- Celebrity.java
+++ Celebrity.java
@@ -1,5 +1,6 @@
 public class Celebrity {
     public static void main(String[] args) {
         System.out.println("vietnam khoobsoorat jaga hain");
+        //ye kabhi sara ali khan ka boyfriend hua karta tha
     }
 }

212a1f4b84da80e3477e728e54552727fb493ff7
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 17:08:29 +0800

    a

--- Celebrity.java
+++ Celebrity.java
@@ -1,5 +1,6 @@
 public class Celebrity {
     public static void main(String[] args) {
         System.out.println("muh ke bal mat gir madarchod");
+        System.out.println("main is film ko bahut badi banane wala hoon");
     }
 }

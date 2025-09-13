bdb7f21d3d6422be82d591885ca3cd27f72af8a4
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 18:32:56 +0800

    a

--- RotliRakabi.java
+++ RotliRakabi.java
@@ -2,7 +2,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class RotliRakabi {
     public static void main(String[] args) {
-        System.out.println("cartoon network");
+        System.out.println("cartoon network - thappad marungic");
     }
 
     public static void main2(String[] args) {

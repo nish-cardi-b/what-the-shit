60619d1b14ec6a31b008864620a32eae54c8a43f
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 16:58:11 +0800

    ha

--- Celebrity.java
+++ Celebrity.java
@@ -1,5 +1,6 @@
 public class Celebrity {
     public static void main(String[] args) {
         System.out.println("farah khali baithi hain");
+        System.out.println("main woh bol hi nahi raha hoon");
     }
 }

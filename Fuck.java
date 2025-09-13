b45e9f0b75e052c3f199227be36bb12ef4645d27
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 16:50:03 +0800

    h2

--- Fuck.java
+++ Fuck.java
@@ -1,5 +1,6 @@
 public class Fuck {
     public static void main(String[] args) {
         System.out.println("fuck");
+        System.out.println("trump ko unhone sidha plane de diya tha!");
     }
 }

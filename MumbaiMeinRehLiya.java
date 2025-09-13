f77165d4bf87ea3a1dd5320f82668d937a9707c9
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 18:11:00 +0800

    Alan Tan Kwan Boon

--- MumbaiMeinRehLiya.java
+++ MumbaiMeinRehLiya.java
@@ -1,5 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class MumbaiMeinRehLiya {
     public static void main(String[] args) {
         System.out.println("janam bhoomi");
+    }
+
+    private static void shitInYourMouth() {
+
     }
 }

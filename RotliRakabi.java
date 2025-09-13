4ccf23f46d059601493853d3a354093f11c59477
nish-cardi-b <cardonishb@gmail.com>
2025-09-13 18:25:49 +0800

    ah

--- RotliRakabi.java
+++ RotliRakabi.java
@@ -1,5 +1,11 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class RotliRakabi {
     public static void main(String[] args) {
         System.out.println("cartoon network");
+    }
+
+    public static void main2(String[] args) {
+        System.out.println("i am on aramdayak u pansy bitch");
     }
 }
